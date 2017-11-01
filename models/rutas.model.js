8488871b32c03f294ec6b95a80decd9b40fa14ef
Caroelena Villela Escamilla <caroelenavillelaescamilla@MacBook-Pro-de-CaroElena.local>
2017-10-31 21:22:37 -0700

    Tarea2.4

--- models/rutas.model.js
+++ models/rutas.model.js
@@ -1,6 +1,4 @@
-/**
- * Created by desarrollo-001 on 31/08/17.
- */
+
 const mongoose = require('mongoose');
 
 const rutaSchema = new mongoose.Schema({
@@ -10,8 +8,6 @@ const rutaSchema = new mongoose.Schema({
         required: true
     },
     camion:[{type:Schema.ObjectId, ref:'Camiones'}]
-
-
 });
 
 const rutaModel = mongoose.model('Ruta', userSchema, 'ruta');
